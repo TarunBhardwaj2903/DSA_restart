51daa5c796b243900a7ae5105f7fb3b0c0c02d82
tarunbhardwaj2005 <tarunbhardwaj2903@gmail.com>
2026-06-23 12:27:46 +0530

    Time: 3 ms (30.88%) | Memory: 19.4 MB (73.14%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,22 +1,30 @@
 class Solution:
-    def is_valid(self,s):
-        balance=0
-        for i in s:
-            if i == "(" : 
-                balance+=1
-            else:
-                balance -=1
-            if balance<0:
-                return False
-        return balance == 0 
-    def genrate_all(self,curr,n,res):
-        if len(curr)== 2*n:
-            if self.is_valid(curr):
-                res.append(curr)
+    # def is_valid(self,s):
+    #     balance=0
+    #     for i in s:
+    #         if i == "(" : 
+    #             balance+=1
+    #         else:
+    #             balance -=1
+    #         if balance<0:
+    #             return False
+    #     return balance == 0 
+    # def genrate_all(self,curr,n,res):
+    #     if len(curr)== 2*n:
+    #         if self.is_valid(curr):
+    #             res.append(curr)
+    #         return
+    #     self.genrate_all(curr+"(",n,res)
+    #     self.genrate_all(curr+")",n,res)
+    def backtrack(self,curr,open,close,n,res):
+        if len(curr)==2*n:
+            res.append(curr)
             return
-        self.genrate_all(curr+"(",n,res)
-        self.genrate_all(curr+")",n,res)
+        if open < n :
+            self.backtrack(curr+"(",open+1,close,n,res)
+        if close < open :
+            self.backtrack(curr+")",open,close+1,n,res)
     def generateParenthesis(self, n: int) -> List[str]:
         res=[]
-        self.genrate_all("",n,res)
+        self.backtrack("",0,0,n,res)
         return res
